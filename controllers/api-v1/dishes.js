c6f157e463861b8cc6c5d6b9b171f3cf64b5ed7a
Billy Lu <billyfwlu@gmail.com>
2022-06-27 15:58:16 -0700

    👌 IMPROVE: improving search component to return nothing

--- controllers/api-v1/dishes.js
+++ controllers/api-v1/dishes.js
@@ -11,20 +11,26 @@ router.get('/search/:dishName', async (req, res) => {
         // find all posts
         const foundDishes = await db.Dish.findOne({dishName: req.params.dishName})
         .populate({
-            path: "posts",
-            populate: [{
-                path: "dish",
-                populate: {
-                    path: "restaurant"
-                }
-            }, {
-                path: "poster",
-                select: "userName"
-            }]
-        })
-        console.log(foundDishes)
+                path: "posts",
+                populate: [{
+                    path: "dish",
+                    populate: {
+                        path: "restaurant"
+                    }
+                }, {
+                    path: "poster",
+                    select: "userName"
+                }]
+            })
+
+            // console.log(foundDishes)
+            if (foundDishes !== null) {
+                res.status(200).json(foundDishes)
+            } else {
+                res.status(204).json(foundDishes)
+            }
+
         // send to the client
-        res.status(200).json(foundDishes)
     } catch (err) {
         res.status(500).json({ msg: 'server error' })
     }
